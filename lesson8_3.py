d8c9ce8066097f4cadee1ded70db8d9421d4283e
xprodigy <85904793+xprodigy@users.noreply.github.com>
2021-07-09 21:03:44 +0300

    lesson 7

--- lesson8_3.py
+++ lesson8_3.py
@@ -1,3 +1,35 @@
-print('Lesson 8_3:')
+print('Lesson 8_3')
 
-# programming now, please wait
+
+class not_numeric(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return f'Warning! Element "{self.msg}" is not numeric'
+
+
+list_in = input('Input numbers separated by space and q instead of the last:\n'
+                'For example: 3 231 31\nFor example: 3365 2 463 q\n>').split(" ")
+
+list_out = []
+stop = False
+
+while True:
+    for el in list_in:
+        if el == 'q':
+            print(list_out)
+            stop = True
+            break
+        elif el.isdigit():
+            list_out.append(int(el))
+        elif el == '':
+            continue
+        else:
+            try:
+                raise not_numeric(el)
+            except not_numeric as err:
+                print(err)
+    if stop:
+        break
+    list_in = input('Enter more >').split(" ")
