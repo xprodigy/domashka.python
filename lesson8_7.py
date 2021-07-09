da4231b8ef98a51e09918b3fe7f37bb786bc56fe
xprodigy <85904793+xprodigy@users.noreply.github.com>
2021-07-10 01:51:56 +0300

    lesson 8

--- lesson8_7.py
+++ lesson8_7.py
@@ -11,8 +11,8 @@ class MyComlex:
     def __mul__(self, other):
         return MyComlex(self.complex_num * other.complex_num)
 
-    def __truediv__(self, other):
-        return MyComlex(self.complex_num / other.complex_num)
+    def __add__(self, other):
+        return MyComlex(self.complex_num + other.complex_num)
 
     def __str__(self):
         return str(self.complex_num)
@@ -24,5 +24,5 @@ arg_3 = MyComlex(1.5 - 1.1j)
 arg_4 = MyComlex(1.2 + 2.1j)
 
 print(arg_1 * arg_2)
-print(arg_1 / arg_2)
-print(arg_1 * arg_2 / arg_3 * arg_4)
+print(arg_1 + arg_2)
+print(arg_1 * arg_2 + arg_3 * arg_4)
